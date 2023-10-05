42acba4c2e94162870c56dcbc81f5bdd56b7a518
Luis Adames <adames1601@gmail.com>
2023-10-04 23:23:42 -0400

    ancho y alto arreglado

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -33,6 +33,7 @@ const CarCard = ({ car }: CarCardProps) => {
       <div className='relative w-full h-40 my-3
       object-contain'>
         <Image src={generateCarImageUrl(car)} alt='car model' fill priority
+        
         className='object-contain'/>
       </div>
       
