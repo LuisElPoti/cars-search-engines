23890b4bca0d3f102e93ef61396a22d19b777cff
Luis Adames <adames1601@gmail.com>
2023-10-12 22:22:29 -0400

    Metadata arreglada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import Image from 'next/image'
 import { Footer, Hero, Navbar, SearchBar } from '@/components'
 import { Metadata } from 'next'
-
+import Head from 'next/head'
 
 
 export default function Home() {
+  const title = 'CarSearch';
+  const description = 'Explora los carros que desees';
+  const logoUrl = 'car-search.ico';
+
   return (
     <main className='overflow-hidden'>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <link rel='icon' href={logoUrl} />
+      </Head>
       <Hero />
+
       <div className='mt-12 padding-x
       padding-y max-width' id='discover'>
         <div className='home__text-container'>
